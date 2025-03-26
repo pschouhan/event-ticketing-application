044e3ac8b479653724c995d0a1a7b94532d2c5eb
pschouhan <pschouhan@gmail.com>
2025-03-26 20:47:18 +0000

    Update database.config.ts with env configurations

--- EventTicketApp/ticketing-backend/src/config/database.config.ts
+++ EventTicketApp/ticketing-backend/src/config/database.config.ts
@@ -1,14 +1,16 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { config } from 'dotenv';
 import { Order } from '../modules/orders/order.entity';
 import { Event } from '../modules/events/event.entity';
+config();
 
 export const DatabaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 5434,
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || '8788',
-  database: process.env.DB_NAME || 'ticketing_db',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: String(process.env.DB_PASS),
+  database: process.env.DB_NAME,
   entities: [Event, Order], 
   synchronize: true,
   autoLoadEntities: true,
